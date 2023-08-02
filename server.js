5c57d40ab191b68dddf6040b39115dd0ac61134b
Gary Grant <garygrant693@gmail.com>
2023-08-02 01:17:29 -0400

    added deployment code

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path:"./config.env"});
+const path = require("node:path");
 const PORT  = process.env.PORT || 5000;
 const db = process.env.DATABASE
 const app = require('./app');
 
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(`./client/build`));
+ 
+    app.get('*',(req,res)=>{
+       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+ }
 
 // mongoose connect 
 mongoose.connect(db,{
