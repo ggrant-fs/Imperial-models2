882112c28571d0f70f1b11e14450eafb264e3ea2
Gary Grant <garygrant693@gmail.com>
2023-08-14 22:29:30 -0400

    changed the px to rem in FeaturedImg.css

--- client/src/Component/UI/FeaturedImg.js
+++ client/src/Component/UI/FeaturedImg.js
@@ -2,17 +2,17 @@ import { Fragment } from 'react';
 import React from 'react';
 import imageOne from '../../images/featuredimg3.jpg';
 import imageTwo from '../../images/featuredimg4.jpg';
-import styles from './FeaturedImg.module.css';
+import './FeaturedImg.css';
 
 const FeaturedImg = () =>{
     return (
         <Fragment>
-            <div className={styles['feature-photos']}>
-               <div className={styles['feature-photos__container']}>
-                   <img src={imageOne} alt='first feature img' className={styles['feature-photos__img1']}/>
+            <div className='feature-photos'>
+               <div className='feature-photos__container'>
+                   <img src={imageOne} alt='first feature img' className='feature-photos__img1'/>
                </div>
-               <div className={styles['feature-photos__container']}>
-                  <img src={imageTwo} alt='second feature img' className={styles['feature-photos__img2']}/>
+               <div className='feature-photos__container'>
+                  <img src={imageTwo} alt='second feature img' className='feature-photos__img2'/>
                </div>
             </div>
         </Fragment>
