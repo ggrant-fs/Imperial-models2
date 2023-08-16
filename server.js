c4aabae86b94d38f920fc19253f4075f08feb057
Gary Grant <garygrant693@gmail.com>
2023-08-16 01:15:28 -0400

    update server.js for heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path:"./config.env"});
 const path = require("node:path");
-const PORT  = process.env.PORT || 5000;
+// const PORT  = process.env.PORT || 5000;
 const db = process.env.DATABASE
 // const app = require('./app');
 
 const app = express();
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(__dirname,`client/build`));
+    app.use(express.static(`client/build`));
  
     app.get('*',(req,res)=>{
        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV==='production'){
 //     useCreateIndex:true, 
 //     useFindAndModify:false 
 // }).then(()=> console.log('Mongo DB connection was successful'))
-
+const PORT  = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`)
 })
